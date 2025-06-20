33849746afa70b8ab6c4a9eacc3423dd5b521e2d
raj9933 <rajendra99065@gmail.com>
2025-06-20 14:00:39 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'blocklywebsite';
+
+  formshow=false;
+
+  showform(){
+    this.formshow=!this.formshow;
+  }
+
 }
